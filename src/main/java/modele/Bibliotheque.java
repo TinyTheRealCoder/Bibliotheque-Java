073a021ec825b61e88f1e23128cd1888b4bf5719
Arthur_Tougma <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-02-18 10:24:35 +0100

    methode consulter_exemplaire_ouvrage

--- src/main/java/modele/Bibliotheque.java
+++ src/main/java/modele/Bibliotheque.java
@@ -2,6 +2,7 @@ package modele;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -44,12 +45,20 @@ private void lier_lecteur(Lecteur l, Integer num) {
     this.lecteurs.put(num, l);
 }
 
-public void consulter_exemplaire_ouvrage(){
-    
-}
-
-public void consulter_lecteur(){
-    
+public void consulter_exemplaire_ouvrage(IHM ihm){
+    ArrayList<String> numerosISBN = new ArrayList<>();
+    numerosISBN = get_numeros_ISBN();
+    String numeroISBN = IHM.saisir_numero_ouvrage(numerosISBN);
+    Ouvrage ouvrage = get_ouvrage(numeroISBN);
+    String titre = ouvrage.get_titre();
+    ihm.afficher_ouvrage(titre, numeroISBN);
+    ArrayList<Exemplaire> exemplaires = new ArrayList<>();
+    exemplaires = get_exemplaires();
+    int numeroExemplaire;
+    for(Exemplaire ex : exemplaires){
+        numeroExemplaire = ex.get_numero();
+        ihm.afficher_numero_exemplaire(numeroExemplaire);
+    }
 }
 
 }
