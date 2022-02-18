6eb0bbe05c968dc82ff6ffde07ce38117f167024
Arthur_Tougma <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-02-18 11:37:18 +0100

    Ajout methode nouvel_exemplaire() dans la classe Bibliotheque

--- src/main/java/modele/Bibliotheque.java
+++ src/main/java/modele/Bibliotheque.java
@@ -48,6 +48,10 @@ public class Bibliotheque implements Serializable {
     
     /* SETTER */
     
+    private void add_lecteur(Lecteur l, Integer num) {
+        this.lecteurs.put(num, l);
+    }
+    
     public void nouveau_lecteur(IHM ihm) {
         IHM.InfosLecteur infosLecteur = ihm.saisir_lecteur();
         Lecteur l = lecteurs.get(infosLecteur.num);
@@ -58,30 +62,6 @@ public class Bibliotheque implements Serializable {
 
         } else {
             ihm.informer_utilisateur("numéro de lecteur existant", false);
-        }
-    }
-
-    private void add_lecteur(Lecteur l, Integer num) {
-        this.lecteurs.put(num, l);
-    }
-
-    private void lier_lecteur(Lecteur l, Integer num) {
-        this.lecteurs.put(num, l);
-    }
-
-    public void consulter_exemplaire_ouvrage(IHM ihm){
-        ArrayList<String> numerosISBN = new ArrayList<>();
-        numerosISBN = this.get_numeros_ISBN();
-        String numeroISBN = ihm.saisir_numero_ouvrage(numerosISBN);
-        Ouvrage ouvrage = get_ouvrage(numeroISBN);
-        String titre = ouvrage.get_titre();
-        ihm.afficher_ouvrage(titre, numeroISBN);
-        ArrayList<Exemplaire> exemplaires = new ArrayList<>();
-        exemplaires = ouvrage.get_exemplaires();
-        int numeroExemplaire;
-        for(Exemplaire ex : exemplaires){
-            numeroExemplaire = ex.get_numero_exemplaire();
-            ihm.afficher_numero_exemplaire(numeroExemplaire);
         }
     }
     
@@ -102,4 +82,22 @@ public class Bibliotheque implements Serializable {
         ouvrage.add_exemplaire(infosExemplaire.quantiteExemplaire, infosExemplaire.quantiteEmpruntable, infosExemplaire.dateReception);
         ihm.informer_utilisateur("Exemplaires ajoutés", true);
     }
+
+
+    public void consulter_exemplaire_ouvrage(IHM ihm){
+        ArrayList<String> numerosISBN = new ArrayList<>();
+        numerosISBN = this.get_numeros_ISBN();
+        String numeroISBN = ihm.saisir_numero_ouvrage(numerosISBN);
+        Ouvrage ouvrage = get_ouvrage(numeroISBN);
+        String titre = ouvrage.get_titre();
+        ihm.afficher_ouvrage(titre, numeroISBN);
+        ArrayList<Exemplaire> exemplaires = new ArrayList<>();
+        exemplaires = ouvrage.get_exemplaires();
+        int numeroExemplaire;
+        for(Exemplaire ex : exemplaires){
+            numeroExemplaire = ex.get_numero_exemplaire();
+            ihm.afficher_numero_exemplaire(numeroExemplaire);
+        }
+    }
+    
 }
